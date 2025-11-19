c230708d07278b4c49c7e1e521a1be9185bd231d
Gina Souv <georgiasouval@gmail.com>
2025-11-19 11:01:51 +0000

    Add exp004: frozen COCO-pretrained detector with class mapping

--- modules/hooks/__init__.py
+++ modules/hooks/__init__.py
@@ -1,7 +1,8 @@
 from .freeze_detector import FreezeDetectorHook
 from .validation_debug_hook import ValidationDebugHook
 from .save_batch_images import SaveBatchImagesHook
+from .class_mapping_validation import ClassMappingValidationHook
 
 
 
-__all__ = ['FreezeDetectorHook', 'ValidationDebugHook', 'SaveBatchImagesHook']
+__all__ = ['FreezeDetectorHook', 'ValidationDebugHook', 'SaveBatchImagesHook', 'ClassMappingValidationHook']

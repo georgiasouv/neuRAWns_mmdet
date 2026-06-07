56af3bbc01cc99805b97705814a57a6557b3a52c
georgiasouval <georgiasouval@gmail.com>
2026-06-07 23:43:39 +0100

    git error

--- modules/raw_preprocessors/__init__.py
+++ modules/raw_preprocessors/__init__.py
@@ -3,30 +3,22 @@ from .conv_gamma import ConvGamma
 from .conv_gamma_gain import ConvGammaGain
 from .conv_log import ConvLog
 from .conv_power import ConvPower
-<<<<<<< HEAD
 from .global_curve import LearnableToneCurve
 from .guided_localTMO import GuidedLocalToneMap
 from .log_gamma_stack import LogGammaStack
 from .residual_refiner import ResidualRefiner
-=======
 from .local_tone import LocalAffine, LocalCurve, LocalHybrid
->>>>>>> 46ee2e3 (local_tone_mapper)
 
-__all__ = ['BasePreprocessor', 
+__all__ = ['BasePreprocessor',
            'ConvGamma',
            'ConvGammaGain',
            'ConvLog',
            'ConvPower',
-<<<<<<< HEAD
            'LearnableToneCurve',
            'GuidedLocalToneMap',
            'LogGammaStack',
-           'ResidualRefiner'
-=======
+           'ResidualRefiner',
            'LocalAffine',
            'LocalCurve',
            'LocalHybrid'
->>>>>>> 46ee2e3 (local_tone_mapper)
            ]
-
-    
